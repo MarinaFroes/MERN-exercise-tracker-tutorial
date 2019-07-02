468f459a3d336cebdcad4baca92d9e2df614cd8f
MFAC <ninafroes@gmail.com>
2019-07-02 20:21:30 +0200

    docs: Add a comment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-  //Allow us to parse JSON
+  //Allow us to parse JSON - our server is going to be sending and receiving json
 app.use(express.json());
 
 // Add uri with enviromental variable from the mongoDB dashboard
