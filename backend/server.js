e63997b14915a8785cb55a154b8a5673ce0d36fa
MFAC <ninafroes@gmail.com>
2019-07-02 17:17:24 +0200

    feat: Add mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 // Require everything we need
 const express = require('express');
 const cors = require('cors');
+// Connect to the mongoDB Atlas database
+const mongoose = require('mongoose');
 
-// Add environment variables in the dotenv file
+// Add environmental variables in the dotenv file
 require('dotenv').config();
 
 // Create the express server
@@ -13,6 +15,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
   //Allow us to parse JSON
 app.use(express.json());
+
+// Add uri with enviromental variable from the mongoDB dashboard
+const uri = process.env.ATLAS_URI;
+// uri - that's where our database is stored
+  // useNewUrlParser and useCreateIndex flags - deal with updates to mongoDB 
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log(`MongoDB database connected successfully`);
+});
 
 // Start the server
 app.listen(port, () => {
