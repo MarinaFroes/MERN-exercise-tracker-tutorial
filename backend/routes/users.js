d75443d74b98b9b515321fc40048b290e88ca076
MFAC <ninafroes@gmail.com>
2019-07-03 10:04:28 +0200

    feat: Add update, delete and get by id

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -22,7 +22,32 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-module.exports = router;
+// Get the info only of a specific id
+// '/:id' is a variable - id is automatically created by mongoDB
+router.route('/:id').get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
-// TODO: In a real production app, it would have a update and a delete
-// We can also do that in the mongoDB Atlas dashboard
+// Deletes only the object associated to the id
+router.route('/:id').delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User deleted'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Update
+router.route('/update/:id').post((req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      user.username = req.body.username;
+      
+      user.save()
+        .then(() => res.json('User updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
