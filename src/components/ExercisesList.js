acaa93e60dafb5a768faf4e13aa476009d0f5d33
MFAC <ninafroes@gmail.com>
2019-07-03 14:03:07 +0200

    feat: Add GET request to database

--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+//import { Link } from "react-router-dom";
+import Axios from "axios";
 
 class ExercisesList extends Component {
+  state = {
+    exercises: []
+  }
+
+  componentDidMount() {
+    Axios.get("http://localhost:5000/exercises/")
+      .then(res => {
+        this.setState({
+          exercises: res.data
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div>
-        <p>ExercisesList Component</p>
+        
       </div>
     )
   }
