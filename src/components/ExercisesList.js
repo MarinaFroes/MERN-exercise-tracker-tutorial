94e13219bfdef67f73aab5a119fed611eb63eda7
MFAC <ninafroes@gmail.com>
2019-07-03 17:04:02 +0200

    feat: Add table

--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -51,7 +51,21 @@ class ExercisesList extends Component {
   render() {
     return (
       <div>
-        
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.getExerciseList()}
+          </tbody>
+        </table>
       </div>
     )
   }
