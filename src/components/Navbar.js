b962d4e3c3f60e364c5780070021d913b3ac51e0
MFAC <ninafroes@gmail.com>
2019-07-03 12:32:17 +0200

    fix: Fix typo on onChange attribute

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
       // Navbar from bootstrap documentation with a few adjustments
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">Excercise Tracker</Link>
-        <div className="collapse navbar-collapse">
+        <div className="collpase navbar-collapse">
           <ul className="navbar-nav mr-auto">
             <li className="navbar-item">
               <Link to="/" className="nav-link">Exercises</Link>
