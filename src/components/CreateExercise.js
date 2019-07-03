fb1752112a792b6f78e7e6ecb7d9940b1aa94dc8
MFAC <ninafroes@gmail.com>
2019-07-03 12:03:18 +0200

    feat: Add description input

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -66,15 +66,27 @@ class CreateExercise extends Component {
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Username:</label>
-            <select ref="userInput" required className="form-control" value={this.state.username} onCHange={this.onChangeUsername}>{this.state.users.map(user => {
-              return (
+            <select ref="userInput" required className="form-control" value={this.state.username} onCHange={this.onChangeUsername}>{
+              this.state.users.map(user => 
+                (
                 <option key={user}
                   id={user}
                   value={user}>
                   {user}</option>
+                )
               )
-            })}
+            }
             </select>
+          </div>
+          <div className="form-group">
+            <label>Description:</label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
           </div>
         </form>
       </div>
