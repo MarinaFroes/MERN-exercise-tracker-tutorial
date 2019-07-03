01554aaadfd07dd478f9f1a02b30d8321e7c50d5
MFAC <ninafroes@gmail.com>
2019-07-03 12:08:27 +0200

    feat: Add DatePicker file

---
+++ src/components/DatePicker.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class DatePicker extends Component {
+  render() {
+    return (
+      <p>DatePicker will be here</p>
+    )
+  }
+}
+
+export default DatePicker;
