d196d9316c53d1b450b4190b74bb8dcf8464bb54
MFAC <ninafroes@gmail.com>
2019-07-03 14:08:47 +0200

    feat: Add delete exercise method

--- src/components/ExercisesList.js
+++ src/components/ExercisesList.js
@@ -7,6 +7,26 @@ class ExercisesList extends Component {
     exercises: []
   }
 
+  deleteExercise = id => {
+    Axios.delete(`http://localhost:5000/exercises/${id}`)
+      .then(res => console.log(res.data));
+    this.setState({
+      // Filter only exercises with a different id than the deleted exercise and save it to this.state.exercises array
+      exercises: this.state.exercises.filter(exercise => exercise._id !== id)
+    })
+  }
+
+  /* Database user format
+    - Access just the id field: exercise._id
+    
+    {
+      "_id":{"$oid":"5d1c4af634019169f344604d"},
+      "username":"beau",
+      "createdAt":{"$date":{"$numberLong":"1562135286872"}},
+      "updatedAt":{"$date":{"$numberLong":"1562135286872"}},
+      "__v":{"$numberInt":"0"}}
+    */
+
   componentDidMount() {
     Axios.get("http://localhost:5000/exercises/")
       .then(res => {
